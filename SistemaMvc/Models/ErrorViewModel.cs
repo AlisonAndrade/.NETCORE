ba878d8dceed1f8b81854325a7a809538598bb44
AlisonAndrade <alisonaparecidoandrade@hotmail.com>
2021-03-22 12:42:05 -0300

    Atualizacao001

---
+++ SistemaMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
